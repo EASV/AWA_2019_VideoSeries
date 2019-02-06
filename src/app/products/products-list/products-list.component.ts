0ce6f708bf631a1df844cf551c8fb5de97ee3003
lbilde <ljb@easv.dk>
2019-02-06 09:20:17 +0100

    adding some access to firebase CRUD

--- src/app/products/products-list/products-list.component.ts
+++ src/app/products/products-list/products-list.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Observable, Subscription} from 'rxjs';
+import {ProductService} from '../shared/product.service';
+import {Product} from '../shared/product.model';
 @Component({
   selector: 'app-products-list',
   templateUrl: './products-list.component.html',
   styleUrls: ['./products-list.component.scss']
 })
 export class ProductsListComponent implements OnInit {
-
-  constructor() { }
+  products: Observable<Product[]>;
+  constructor(private ps: ProductService) { }
 
   ngOnInit() {
+    this.products = this.ps.getProducts();
+  }
 
+  updateProduct(product: Product) {
+    product.name = product.name + '1';
+    this.ps.update(product);
+  }
+
+  deleteProduct(product: Product) {
+    this.ps.delete(product);
+  }
+
+  addProduct() {
+    this.ps.add({name: 'New Product'});
+  }
+
+  getDetails(product: Product) {
+    this.ps.getProduct(product.id);
   }
 
 }
--- src/app/products/shared/product.service.ts
+++ src/app/products/shared/product.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import {AngularFirestore} from '@angular/fire/firestore';
+import {Observable} from 'rxjs';
+import {Product} from './product.model';
+import {map, tap} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -7,4 +10,67 @@ import {AngularFirestore} from '@angular/fire/firestore';
 export class ProductService {
 
   constructor(private db: AngularFirestore) { }
+
+  getProducts(): Observable<Product[]> {
+    return this.db
+      .collection<Product>('products')
+      // This will return an Observable
+      .snapshotChanges()
+      .pipe(
+        map(actions => {
+          // actions is an array of DocumentChangeAction
+          /*
+          const prods: Product[] = [];
+          actions.forEach(action => {
+            const data = action.payload.doc.data() as Product;
+            prods.push({
+              id: action.payload.doc.id,
+              name: data.name
+            });
+          });
+          return prods;
+          */
+
+          return actions.map(action => {
+            const data = action.payload.doc.data() as Product;
+            return {
+              id: action.payload.doc.id,
+              name: data.name
+            };
+          });
+        })
+      );
+  }
+
+  getProduct(id: string) {
+    this.db.doc<Product>('products/' + id).get()
+      .subscribe(productFound => {
+
+        const protData = productFound.data() as Product;
+      });
+  }
+
+  update(product: Product) {
+    this.db.doc<Product>('products/' + product.id)
+      .update(product)
+      .then(prod => {
+        debugger;
+      });
+  }
+
+  delete(product: Product) {
+    this.db.doc<Product>('products/' + product.id)
+      .delete()
+      .then(prod => {
+        debugger;
+      });
+  }
+
+  add(product: Product) {
+    this.db.collection<Product>('products')
+      .add(product)
+      .then(prod => {
+        debugger;
+      });
+  }
 }
