f4b5f00a3829256b56463c124337d5ec186752ab
lbilde <ljb@easv.dk>
2019-02-16 10:19:03 +0100

    Adding files module

---
+++ src/app/files/shared/file.ts
@@ -0,0 +1,7 @@
+export interface File {
+  id?: string;
+  name: string;
+  type: string;
+  size: number;
+  lastChanged?: Date;
+}
--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductsListComponent } from './products-list/products-list.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import {FilesModule} from '../files/files.module';
 
 @NgModule({
   declarations: [ProductsListComponent],
   imports: [
     CommonModule,
     ProductsRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FilesModule
   ]
 })
 export class ProductsModule { }
